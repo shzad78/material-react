876d60628c1202d3cc7972c032c3c54f1cec0555
shzad78 <shzad78@gmail.com>
2020-02-06 15:16:55 +0100

    AppBar

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+
+export default function Header() {
+    return (
+        <AppBar>
+        <Toolbar> Arc development</Toolbar> 
+        </AppBar>
+    )
+}
