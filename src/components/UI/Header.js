52af9dabd47b4115ca2fa7219659eb9c18feab81
shzad78 <shzad78@gmail.com>
2020-02-26 13:42:28 +0100

    menu

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -6,6 +6,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import logo from '../../assets/logo.svg';
 
@@ -33,9 +35,17 @@ export default function Header(props) {
 
     const classes = useStyles()
     const [value, setValue] = useState (1);
+    const [anchorEl, setAnchorEl ] = useState(null);
     const handleChange = (e, value) => {
         setValue (value)
     }
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
     
     return (
         <React.Fragment>
@@ -46,12 +56,26 @@ export default function Header(props) {
          <Tabs  value = {value} onChange = {handleChange}
          className = {classes.tabContainer} indicatorColor = "secondary">
          <Tab className = {classes.tab} label="Home" component = {Link} to = "/" />
-         <Tab className = {classes.tab} label="Services" component = {Link} to = "/services" />
+         <Tab aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}
+         className = {classes.tab} label="Services" component = {Link} to = "/services" />
          <Tab className = {classes.tab} label="The Revolution" component = {Link} to = "/revolution" />
          <Tab className = {classes.tab} label="About us" component = {Link} to = "/about" />
          <Tab className = {classes.tab} label="Contact" component = {Link} to = "/contact" />
        </Tabs>
-       <Button variant='contained' color = 'secondary' component = {Link} to = "/estimate"> Free Estimate </Button>
+       <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} 
+       variant='contained' color = 'secondary' component = {Link} to = "/estimate"> Free Estimate 
+       </Button>
+       <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
         </Toolbar> 
         </AppBar>
         <div className ={classes.toolbarMargin}/>
